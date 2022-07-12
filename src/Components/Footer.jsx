1506719f3b1849eaf0d716eae2a2640285e195c2
James Onyekachi <okilonet1@gmail.com>
2022-07-12 01:59:49 +0100

    update with bg

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
-import { MdLocationPin } from 'react-icons/md'
-import { BsInstagram, BsTwitter } from 'react-icons/bs'
-import { FaLinkedin } from 'react-icons/fa'
-import { HiMail } from 'react-icons/hi'
-import './Footer.css'
-
+import React from "react";
+import { MdLocationPin } from "react-icons/md";
+import { BsInstagram, BsTwitter } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-        <a class="glassIco" href="https://goo.gl/maps/eH4vxhLr2pq3uscu8" target="_blank"><MdLocationPin/></a>
-        <a class="glassIco" href="mailto:asvaabuad@gmail.com" target="_blank"><HiMail /></a>
-        <a class="glassIco" href="https://www.instagram.com/a.s.v.a__/" target="_blank"><BsInstagram /></a>
-        <a class="glassIco" href="https://www.linkedin.com/company/abuad-salt-valley-associates-asva/" target="_blank"><FaLinkedin /></a>
-        <a class="glassIco" href="https://mobile.twitter.com/asva__" target="_blank"><BsTwitter /></a>
+    <div className="footer-container">
+      <a
+        class="glassIco"
+        href="https://goo.gl/maps/eH4vxhLr2pq3uscu8"
+        target="_blank"
+      >
+        <MdLocationPin />
+      </a>
+      <a class="glassIco" href="mailto:asvaabuad@gmail.com" target="_blank">
+        <HiMail />
+      </a>
+      <a
+        class="glassIco"
+        href="https://www.instagram.com/a.s.v.a__/"
+        target="_blank"
+      >
+        <BsInstagram />
+      </a>
+      <a
+        class="glassIco"
+        href="https://www.linkedin.com/company/abuad-salt-valley-associates-asva/"
+        target="_blank"
+      >
+        <FaLinkedin />
+      </a>
+      <a
+        class="glassIco"
+        href="https://mobile.twitter.com/asva__"
+        target="_blank"
+      >
+        <BsTwitter />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
